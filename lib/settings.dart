630ea2ba15e37688b25bba48b4f765d55643e83c
sun30812 <sun30812@naver.com>
2021-05-17 09:16:39 +0900

    설정 페이지 로직 일부 수정

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -25,14 +26,36 @@ class _SettingsState extends State<Settings> {
         'settings: $index', _settingsData[index] ?? false);
   }
 
-  void switchClick(int index, bool value) {
+  void switchClick(int index, bool value, bool? isRequiredRestart) {
     setState(() {
       _settingsData[index] = value;
     });
     setSettingsData(index);
+    if (isRequiredRestart == true) {
+      showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text('안내'),
+            content: Text('설정을 위해 앱을 지금 종료합니다.'),
+            actions: [
+              CupertinoDialogAction(
+                child: Text('확인'),
+                isDefaultAction: true,
+                onPressed: () => exit(0),
+              )
+            ],
+          );
+        },
+      );
+    }
   }
 
-  GestureDetector settingsItem(int index, String data, String infoData) {
+  GestureDetector settingsItem(
+      {required int index,
+      required String data,
+      required String infoData,
+      bool? isRequiredRestart}) {
     return GestureDetector(
       onTap: () {
         showCupertinoDialog(
@@ -65,7 +88,8 @@ class _SettingsState extends State<Settings> {
                 ),
                 CupertinoSwitch(
                     value: _settingsData[index] ?? false,
-                    onChanged: (value) => switchClick(index, value))
+                    onChanged: (value) =>
+                        switchClick(index, value, isRequiredRestart))
               ],
             ),
           )
@@ -90,10 +114,19 @@ class _SettingsState extends State<Settings> {
       ),
       child: ListView(
         children: [
-          settingsItem(0, '설정 아이콘 추가하기(현재 미지원)', '아직 지원되지 않는 기능입니다.'),
-          settingsItem(1, '삭제 시 경고 표시하지 않기', '항목을 지울 시 경고가 뜨지 않습니다.'),
           settingsItem(
-              2, '개발자 모드 활성화', '개발자용 명령어를 앱의 홈화면에서 사용할 수 있게 됩니다.(사용에 주의바람)'),
+              index: 0,
+              data: '설정 아이콘 추가하기(현재 미지원, 앱 재시작 필요)',
+              infoData: '하단 바에 설정 아이콘을 추가합니다. 해당 기능은 앱을 재시작한 후에 적용됩니다.',
+              isRequiredRestart: true),
+          settingsItem(
+              index: 1,
+              data: '삭제 시 경고 표시하지 않기',
+              infoData: '항목을 지울 시 경고가 뜨지 않습니다.'),
+          settingsItem(
+              index: 2,
+              data: '개발자 모드 활성화',
+              infoData: '개발자용 명령어를 앱의 홈화면에서 사용할 수 있게 됩니다.(사용에 주의바람)'),
         ],
       ),
     );
